c1b3e4d9f5686a90725e39e333d591499211922c
Altoid <102745236+Raynnerkavulika@users.noreply.github.com>
2025-07-04 19:03:13 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,25 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login page</title>
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+    <!-- cdn js file link -->
+     <link rel="stylesheet" href="">
+</head>
+<body>
+    <section class="form-container">
+        <form action="" method="post">
+            <h3>welcome back</h3>
+            <input type="email" placeholder="enter your email" class="box" required name="email">
+            <input type="password" placeholder="enter your password" class="box" required name="password">
+            <input type="submit" name="submit" class="btn" value="login now">
+            <p>don't have an account?<a href="register.php">register here</a>
+            </p>
+        </form>
+    </section>
+</body>
+</html>
